444530ed172e8151addd96576c8c311e5edf3ba9
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-03-16 17:59:04 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,18 +1,35 @@
 import api from "./api";
 
 /* GET ALL PRODUCTS */
-export const getProducts = () => {
-  return api.get("/products");
+
+export const getProducts = (filters = {}, token = null) => {
+
+  const query = new URLSearchParams(filters).toString();
+
+  return api.get(`/products?${query}`, {
+    headers: token
+      ? { Authorization: `Bearer ${token}` }
+      : {}
+  });
+
 };
+
+
 /* CREATE PRODUCT */
+
 export const createProduct = (formData, token) => {
+
   return api.post("/products", formData, {
     headers: {
       Authorization: `Bearer ${token}`
     }
   });
+
 };
+
+
 /* GET SINGLE PRODUCT */
+
 export const getProductById = (id) => {
   return api.get(`/products/${id}`);
 };
