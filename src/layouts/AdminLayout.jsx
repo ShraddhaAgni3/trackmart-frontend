e2888f82bba4887caedd717167072980b88c4661
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-04-06 16:48:40 +0530

    Update AdminLayout.jsx

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -2,32 +2,30 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
+
 export default function AdminLayout() {
   return (
-    <div className="flex min-h-screen bg-bgApp">
+    <div className="min-h-screen bg-bgApp flex">
 
       {/* Sidebar */}
-      <div className="hidden md:block">
-        <Sidebar role="admin" />
-      </div>
+      <Sidebar role="admin" />
 
-      {/* Main Area */}
-      <div className="flex-1 flex flex-col">
+      {/* Main */}
+      <div className="flex-1 flex flex-col md:ml-64">
 
-        {/* Top Navbar */}
         <div className="sticky top-0 z-10">
           <Navbar />
         </div>
 
-        {/* Page Content */}
-        <div className="flex-1 p-10">
-          <div className="bg-bgSurface border border-borderDefault rounded-2xl shadow-card p-8 animate-fade">
+        <div className="flex-1 p-4 md:p-10">
+          <div className="bg-bgSurface border rounded-2xl shadow p-4 md:p-8">
             <Outlet />
           </div>
         </div>
-<Footer/>
+
+        <Footer />
+
       </div>
-      
     </div>
   );
 }
