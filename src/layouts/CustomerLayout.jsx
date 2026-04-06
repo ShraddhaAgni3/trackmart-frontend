ab59ccfc5b5f7d65c710d423d064802b545dadde
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-04-06 17:07:37 +0530

    Update CustomerLayout.jsx

--- src/layouts/CustomerLayout.jsx
+++ src/layouts/CustomerLayout.jsx
@@ -7,11 +7,11 @@ export default function CustomerLayout() {
   return (
     <div className="flex min-h-screen bg-bgApp">
 
-      {/* 🔥 Sidebar always render */}
+      {/* Sidebar */}
       <Sidebar role="customer" />
 
       {/* Main Content Area */}
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col md:ml-64">
 
         {/* Navbar */}
         <div className="sticky top-0 z-10">
