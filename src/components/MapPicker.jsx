d3104035d40eac8e72374b78062c653fe7a08ccb
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-04-13 20:19:46 +0530

    Update MapPicker.jsx

--- src/components/MapPicker.jsx
+++ src/components/MapPicker.jsx
@@ -1,5 +1,5 @@
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
