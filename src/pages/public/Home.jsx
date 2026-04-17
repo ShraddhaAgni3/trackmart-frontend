bb503cb18d7005dff12c3e8c42eba3af0a2e55af
ShraddhaAgni3 <155717879+ShraddhaAgni3@users.noreply.github.com>
2026-04-17 20:03:00 +0530

    Update Home.jsx

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -460,8 +460,14 @@ useEffect(() => {
         <div
           key={i}
           className={`relative h-[80px] rounded-xl overflow-hidden shadow-md bg-gradient-to-r ${ad.bg} flex items-center px-3 py-2`}
-        >
-          <img src={ad.image} className="w-10 h-10 rounded-lg object-cover" />
+        ><img
+  src={ad.image}
+  onError={(e)=>{
+    e.target.onerror = null;
+    e.target.src = "https://res.cloudinary.com/dsn1q7hyk/image/upload/q_auto/f_auto/v1774419499/Clinton-Foodmart_ktkl3m.jpg";
+  }}
+  className="w-12 h-12 rounded-lg object-cover shrink-0"
+/>
           <div className="ml-3 flex-1 flex flex-col justify-center">
             <p className="text-xs font-semibold">{ad.title}</p>
             <p className="text-[10px] text-gray-700">{ad.desc}</p>
@@ -547,7 +553,14 @@ useEffect(() => {
           key={i}
           className={`relative h-[80px] rounded-xl overflow-hidden shadow-md bg-gradient-to-r ${ad.bg} flex items-center px-3 py-2`}
         >
-          <img src={ad.image} className="w-10 h-10 rounded-lg object-cover" />
+         <img
+  src={ad.image}
+  onError={(e)=>{
+    e.target.onerror = null;
+    e.target.src = "https://res.cloudinary.com/dsn1q7hyk/image/upload/q_auto/f_auto/v1774419499/Clinton-Foodmart_ktkl3m.jpg";
+  }}
+  className="w-12 h-12 rounded-lg object-cover shrink-0"
+/>
           <div className="ml-3 flex-1 flex flex-col justify-center">
             <p className="text-xs font-semibold">{ad.title}</p>
             <p className="text-[10px] text-gray-700">{ad.desc}</p>
